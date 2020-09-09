4f4338da98bc658a687a7a0af97400c42a7e99c3
mag389 <mag389@cornell.edu>
2020-09-09 22:22:52 +0000

    removed quoted blocks because count is a class method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@ from api.v1.views import app_views
 from flask import jsonify
 import models
 from models import storage
+from models.amentiy import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", strict_slashes=False)
@@ -19,12 +25,12 @@ def get_stats():
     """Gets stats for models
     """
     stats_dict = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
 
     return jsonify(stats_dict)
