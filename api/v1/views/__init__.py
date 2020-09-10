4a9be6280072fddf13f8ba4cf63ced7273177b46
mag389 <mag389@cornell.edu>
2020-09-10 00:00:31 +0000

    update init on amenity branch

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,13 +6,31 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 if app_views:
     from api.v1.views.index import *
+    try:
+        import api.v1.views.amenities
+    except ImportError as error:
+        pass
+    try:
+        import api.v1.views.states
+    except ImportError as error:
+        pass
+    try:
+        import api.v1.views.cities
+    except ImportError as error:
+        pass
+    try:
+        import api.v1.views.users
+    except ImportError as error:
+        pass
+    try:
+        import api.v1.views.places
+    except ImportError as error:
+        pass
+    try:
+        import api.v1.views.places_reviews
+    except ImportError as error:
+        pass
     """
-    from api.v1.views.states import *
-    from api.v1.views.cities import *
-    from api.v1.views.amenities import *
-    from api.v1.views.users import *
-    from api.v1.views.places import *
-    from api.v1.views.places_reviews import *
     from api.v1.views.places_amenities import *
     from api.v1.app import *
     """
